e5cc75477d05caa0ad42306bccc390b854947ca4
howlonghasitBen <83527361+howlonghasitBen@users.noreply.github.com>
2025-10-01 00:54:50 -1000

    Deck page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,41 @@ import React, { useState } from "react";
 import Navigation from "./components/Navigation";
 import CardCarousel from "./components/CardCarousel";
 import Footer from "./components/Footer";
+import DeckPage from "./components/DeckPage";
 import "./App.css";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("home");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "deck":
+        return <DeckPage />;
+      case "home":
+      default:
+        return (
+          <>
+            {/* Main content */}
+            <div className="bodyContent">
+              <div id="leftContent">
+                <img
+                  id="frontSvg"
+                  src={`${process.env.PUBLIC_URL}/images/pepe-beach/gifs/input.gif`}
+                  alt="Beach Scene"
+                />
+              </div>
+
+              <div className="rightContentWrapper">
+                <CardCarousel />
+              </div>
+            </div>
+
+            {/* Footer */}
+            <Footer />
+          </>
+        );
+    }
+  };
 
   return (
     <div className="app">
@@ -15,23 +46,8 @@ function App() {
       {/* Navigation system */}
       <Navigation onNavigate={setCurrentPage} currentPage={currentPage} />
 
-      {/* Main content */}
-      <div className="bodyContent">
-        <div id="leftContent">
-          <img
-            id="frontSvg"
-            src={`${process.env.PUBLIC_URL}/images/pepe-beach/gifs/input.gif`}
-            alt="Beach Scene"
-          />
-        </div>
-
-        <div className="rightContentWrapper">
-          <CardCarousel />
-        </div>
-      </div>
-
-      {/* Footer */}
-      <Footer />
+      {/* Page content */}
+      {renderPage()}
     </div>
   );
 }
